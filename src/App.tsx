6915c173bc28f4a9104a13e3b6e7b77697dcb6ed
masaka13 <masaka724.13@gmail.com>
2022-08-14 10:40:29 +0900

    init: urqlを使いgithub apiを利用するまで

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider, useQuery } from "urql";
+import "./App.css";
+import { gqlClient } from "./common/gqlClient";
 
-function App() {
+const token = process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN;
+console.log(token);
+
+const client = gqlClient({ url: "https://api.github.com/graphql", token: token });
+
+type Node = {
+  id?: string;
+  url?: string;
+  name?: string;
+  description?: string;
+  createdAt?: string;
+};
+
+type RepositoryResult = {
+  search: {
+    nodes: Node[];
+  };
+};
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div></div>
+      <Provider value={client}>
+        <Content />
+      </Provider>
     </div>
   );
+};
+
+const query = `
+query RepositoryNameQuery($searchQuery: String!) {
+  search(type: REPOSITORY, query: $searchQuery, last: 20) {
+    repositoryCount
+    nodes {
+      ... on Repository {
+        id
+        url
+        name
+        description
+        createdAt
+      }
+    }
+  }
 }
+`;
+
+const Content = () => {
+  const searchQuery = "React in:readme";
+  // const [result, reexecuteQuery] = useQuery<RepositoryResult>({
+  //   query: query,
+  //   variables: { searchQuery },
+  // });
+  // const { data, fetching, error } = result;
+
+  const [{ fetching, data, error }] = useQuery<RepositoryResult>({
+    query: query,
+    variables: { searchQuery },
+  });
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
+  return (
+    <ul className="list-group">
+      {data &&
+        data.search.nodes.map((r, i) => (
+          <li key={i}>
+            <a href={r.url}>{r.url}</a>
+          </li>
+        ))}
+    </ul>
+  );
+};
 
 export default App;
